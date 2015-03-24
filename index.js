5f9141d7bae3d2fc63834fb7045161485723ee2f
Taylor Blau <me@ttaylorr.com>
2015-03-24 18:03:30 -0400

    Add CounterMetric, associated test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
     Statsd: {
       AbstractMetric: require('./lib/metrics/statsd/abstract_statsd_metric.js'),
       Gauge: require('./lib/metrics/statsd/gauge.js'),
-      // Counter: require('./lib/metrics/statsd/counter.js'),
+      Counter: require('./lib/metrics/statsd/counter.js'),
       // Timer: require('./lib/metrics/statsd/timer.js'),
       // Histogram: require('./lib/metrics/statsd/histogram.js'),
       // Meter: require('./lib/metrics/statsd/meter.js')
